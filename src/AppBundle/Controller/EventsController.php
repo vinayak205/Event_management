372b1ad1844ef98f22e2d96aefd43da283a73509
Vinayak Bharti <vinayak205@gmail.com>
2016-08-23 12:02:11 +0530

    Added Event Entity.

---
+++ src/AppBundle/Controller/EventsController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\User;
+use AppBundle\Entity\Venue;
+use AppBundle\Entity\Event;
+use AppBundle\Form\UserType;
+
+
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class EventsController extends Controller{
+	
+}
+    
