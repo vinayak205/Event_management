2999e73f20a38eaa371782c6361d7538d02ee4ee
Vinayak Bharti <vinayak205@gmail.com>
2016-08-23 11:18:57 +0530

    Added create and list venues.

--- src/AppBundle/Controller/VenueController.php
+++ src/AppBundle/Controller/VenueController.php
@@ -17,7 +17,89 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 
 
+
 class VenueController extends Controller{
+
+	/**
+     * @Route("/venue/list", name="venue_list")
+     */
+    public function listAction(){
+    	try{
+    		$venues = $this->getDoctrine()
+            ->getRepository('AppBundle:Venue')
+            ->findAll();
+
+        	return $this->render('venue/list.html.twig', array(
+                'venues' => $venues
+            ));
+    	}
+    	catch(\Exception $e){
+    		$this->addFlash(
+                'notice',
+                'Error: Not able to list the venues!'
+            );
+            return $this->redirectToRoute('homepage');
+    	}
+        
+    }
+
+    /**
+     * @Route("/venue/add", name="venue_add")
+     */
+    public function createAction(Request $request){
+
+    	try{
+
+    		$venue = new Venue;
+	        $form = $this->createFormBuilder($venue)
+	            ->add('name', TextType::class, array('attr' => 
+	            	array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+	            ->add('description', TextareaType::class, array('attr' => 
+	            	array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+	            ->add('address', TextareaType::class, array('attr' => 
+	            		array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+	            ->getForm();
+
+	        $form -> handleRequest($request);
+
+	        if($form -> isSubmitted() && $form -> isValid()){
+	            $name = $form['name']->getData();
+	            $description = $form['description']->getData();
+	            $address = $form['address']->getData();
+
+	            $venue->setName($name);
+	            $venue->setDescription($description);
+	            $venue->setAddress($address);
+
+	            $em = $this->getDoctrine()->getManager();
+	            $em->persist($venue);
+	            $em->flush();
+
+	            $this->addFlash(
+	                'notice',
+	                'Venue Added'
+	                );
+	            return $this->redirectToRoute('venue_list');
+
+	        }
+
+	        return $this->render('venue/create.html.twig', array(
+	                'form' => $form->createView()
+	            ));
+
+    	}
+    	catch(\Exception $e){
+    		$this->addFlash(
+                'notice',
+                'Error: Venue not added.'
+            );
+            return $this->redirectToRoute('venue_list');
+
+    	}
+
+        
+    }
+    
 
 }
 
