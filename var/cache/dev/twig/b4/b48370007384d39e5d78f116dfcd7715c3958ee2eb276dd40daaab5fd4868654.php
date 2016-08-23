15acba40e41978a8eeaaaf5f81832a4812e467e0
Vinayak Bharti <vinayak205@gmail.com>
2016-08-23 16:39:06 +0530

    Showing event status in venue details page.

--- var/cache/dev/twig/b4/b48370007384d39e5d78f116dfcd7715c3958ee2eb276dd40daaab5fd4868654.php
+++ var/cache/dev/twig/b4/b48370007384d39e5d78f116dfcd7715c3958ee2eb276dd40daaab5fd4868654.php
@@ -21,20 +21,20 @@ class __TwigTemplate_f0be3fddca9b5a293d5b7e2189367b33654b5cd4cfef7b9df64b9aede15
 
     protected function doDisplay(array $context, array $blocks = array())
     {
-        $__internal_ce5f49171efeb77bc1b44e194f0801ee7bb1e517e93e368f6b9583972d1fbb20 = $this->env->getExtension("native_profiler");
-        $__internal_ce5f49171efeb77bc1b44e194f0801ee7bb1e517e93e368f6b9583972d1fbb20->enter($__internal_ce5f49171efeb77bc1b44e194f0801ee7bb1e517e93e368f6b9583972d1fbb20_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "venue/details.html.twig"));
+        $__internal_ccd6d5370705508d58301fbbac1719643a63a87d2d44942dd8e709ca6b4b02c3 = $this->env->getExtension("native_profiler");
+        $__internal_ccd6d5370705508d58301fbbac1719643a63a87d2d44942dd8e709ca6b4b02c3->enter($__internal_ccd6d5370705508d58301fbbac1719643a63a87d2d44942dd8e709ca6b4b02c3_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "venue/details.html.twig"));
 
         $this->parent->display($context, array_merge($this->blocks, $blocks));
         
-        $__internal_ce5f49171efeb77bc1b44e194f0801ee7bb1e517e93e368f6b9583972d1fbb20->leave($__internal_ce5f49171efeb77bc1b44e194f0801ee7bb1e517e93e368f6b9583972d1fbb20_prof);
+        $__internal_ccd6d5370705508d58301fbbac1719643a63a87d2d44942dd8e709ca6b4b02c3->leave($__internal_ccd6d5370705508d58301fbbac1719643a63a87d2d44942dd8e709ca6b4b02c3_prof);
 
     }
 
     // line 2
     public function block_body($context, array $blocks = array())
     {
-        $__internal_9a619c77a6af6dd533ee0a4d80189e8f37a28b31814509753a8599c8ccf995ee = $this->env->getExtension("native_profiler");
-        $__internal_9a619c77a6af6dd533ee0a4d80189e8f37a28b31814509753a8599c8ccf995ee->enter($__internal_9a619c77a6af6dd533ee0a4d80189e8f37a28b31814509753a8599c8ccf995ee_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
+        $__internal_7e54c58e84df584e5349dc88cdb4b9612081cebda2b0d393d684b730de222312 = $this->env->getExtension("native_profiler");
+        $__internal_7e54c58e84df584e5349dc88cdb4b9612081cebda2b0d393d684b730de222312->enter($__internal_7e54c58e84df584e5349dc88cdb4b9612081cebda2b0d393d684b730de222312_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
 
         // line 3
         echo "\t<a class = \"btn btn-default\"href=\"/venue/list\"> Back to List</a>
@@ -60,26 +60,31 @@ class __TwigTemplate_f0be3fddca9b5a293d5b7e2189367b33654b5cd4cfef7b9df64b9aede15
 \t\t\t<th>Event name</th>
 \t\t\t<th>Start Date</th>
 \t\t\t<th>End Date</th>
+\t\t\t<th>Status</th>
 \t\t</thead>
 \t\t<tbody>
 \t\t";
-        // line 19
+        // line 20
         $context['_parent'] = $context;
         $context['_seq'] = twig_ensure_traversable((isset($context["events"]) ? $context["events"] : $this->getContext($context, "events")));
         foreach ($context['_seq'] as $context["_key"] => $context["event"]) {
-            // line 20
+            // line 21
             echo "\t\t\t<tr>
 \t\t\t\t<td>";
-            // line 21
+            // line 22
             echo twig_escape_filter($this->env, $this->getAttribute($context["event"], "name", array()), "html", null, true);
             echo "</td>
 \t\t\t\t<td>";
-            // line 22
+            // line 23
             echo twig_escape_filter($this->env, twig_date_format_filter($this->env, $this->getAttribute($context["event"], "startDate", array()), "m/d/Y"), "html", null, true);
             echo "</td>
 \t\t\t\t<td>";
-            // line 23
+            // line 24
             echo twig_escape_filter($this->env, twig_date_format_filter($this->env, $this->getAttribute($context["event"], "endDate", array()), "m/d/Y"), "html", null, true);
+            echo "</td>
+\t\t\t\t<td>";
+            // line 25
+            echo twig_escape_filter($this->env, $this->getAttribute($context["event"], "status", array()), "html", null, true);
             echo "</td>
 \t\t\t</tr>
 \t\t";
@@ -87,12 +92,12 @@ class __TwigTemplate_f0be3fddca9b5a293d5b7e2189367b33654b5cd4cfef7b9df64b9aede15
         $_parent = $context['_parent'];
         unset($context['_seq'], $context['_iterated'], $context['_key'], $context['event'], $context['_parent'], $context['loop']);
         $context = array_intersect_key($context, $_parent) + $_parent;
-        // line 26
+        // line 28
         echo "\t\t</tbody>
 \t</div>
 ";
         
-        $__internal_9a619c77a6af6dd533ee0a4d80189e8f37a28b31814509753a8599c8ccf995ee->leave($__internal_9a619c77a6af6dd533ee0a4d80189e8f37a28b31814509753a8599c8ccf995ee_prof);
+        $__internal_7e54c58e84df584e5349dc88cdb4b9612081cebda2b0d393d684b730de222312->leave($__internal_7e54c58e84df584e5349dc88cdb4b9612081cebda2b0d393d684b730de222312_prof);
 
     }
 
@@ -108,7 +113,7 @@ class __TwigTemplate_f0be3fddca9b5a293d5b7e2189367b33654b5cd4cfef7b9df64b9aede15
 
     public function getDebugInfo()
     {
-        return array (  91 => 26,  82 => 23,  78 => 22,  74 => 21,  71 => 20,  67 => 19,  53 => 8,  49 => 7,  44 => 5,  40 => 3,  34 => 2,  11 => 1,);
+        return array (  96 => 28,  87 => 25,  83 => 24,  79 => 23,  75 => 22,  72 => 21,  68 => 20,  53 => 8,  49 => 7,  44 => 5,  40 => 3,  34 => 2,  11 => 1,);
     }
 }
 /* {% extends 'base.html.twig' %}*/
@@ -127,6 +132,7 @@ class __TwigTemplate_f0be3fddca9b5a293d5b7e2189367b33654b5cd4cfef7b9df64b9aede15
 /* 			<th>Event name</th>*/
 /* 			<th>Start Date</th>*/
 /* 			<th>End Date</th>*/
+/* 			<th>Status</th>*/
 /* 		</thead>*/
 /* 		<tbody>*/
 /* 		{% for event in events %}*/
@@ -134,6 +140,7 @@ class __TwigTemplate_f0be3fddca9b5a293d5b7e2189367b33654b5cd4cfef7b9df64b9aede15
 /* 				<td>{{event.name}}</td>*/
 /* 				<td>{{event.startDate|date('m/d/Y')}}</td>*/
 /* 				<td>{{event.endDate|date('m/d/Y')}}</td>*/
+/* 				<td>{{event.status}}</td>*/
 /* 			</tr>*/
 /* 		{% endfor %}*/
 /* 		</tbody>*/
